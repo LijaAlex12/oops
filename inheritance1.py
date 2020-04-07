97c7c575eae7e76703a294650f9d2cb2a92f02cd
LijaAlex12 <lijaalexhere@gmail.com>
2020-04-08 00:21:56 +0530

    multilevel inheritance

---
+++ inheritance1.py
@@ -0,0 +1,33 @@
+# superclass
+
+# multilevel inheritance
+class A:
+    def feature1(self):
+        print('feature1')
+
+    def feature2(self):
+        print('feature2')
+
+# subclass
+class B(A):
+    def feature3(self):
+        print('feature3')
+
+    def feature4(self):
+        print('feature4')
+
+class C(B):
+    def feature5(self):
+        print('feature5')
+
+a1=A()
+a1.feature1()
+a1.feature2()
+
+b1=B()
+b1.feature1()
+b1.feature3()
+
+c1=C()
+c1.feature1()
+c1.feature5()
