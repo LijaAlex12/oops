bd6131020df2d407d69be378f088f7efc85cf9d6
LijaAlex12 <lijaalexhere@gmail.com>
2020-04-08 00:40:54 +0530

    default constructor multilevel inheritance

---
+++ constructorInheritance.py
@@ -0,0 +1,27 @@
+class A:
+    def __init__(self):
+        print('nhhb')
+    def feature1(self):
+        print('feature1 A')
+
+    def feature2(self):
+        print('feature2')
+
+# subclass
+class B():
+    def __init__(self):
+        super().__init__()
+        print('b init')
+    def feature1(self):
+        print('feature1 B')
+
+    def feature4(self):
+        print('feature4')
+
+class C(A,B):
+    def __init__(self):
+        super().__init__()
+        super().feature1()
+        print('c init')
+
+a1=C()
